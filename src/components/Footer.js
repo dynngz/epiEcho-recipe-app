128b2d1fdf9472083ccc690d5602bf681150722b
dynngz <dayanagzrd@gmail.com>
2025-03-18 11:10:47 -0500

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ export default function Footer() {
                 <p className="title">Contact us</p>
                 <p>epi-echo@email.com</p> 
                 <p>+51 956513441</p> 
-                <p>1350 Street Castellana</p> 
+                <p>90 Street Castellana</p> 
             </div>
             <div className="footer-section">
                 <p className="title">Social Media</p>
